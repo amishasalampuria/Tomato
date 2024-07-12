ad85f54c75643c8bf4a70504e162011a4bd9d08a
amishasalampuria <91007698+amishasalampuria@users.noreply.github.com>
2024-07-12 23:07:30 +0530

    Update Home.js

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -2,10 +2,10 @@ import React, { useContext, useState } from 'react'
 import './Home.css';
 
 // import '../../components/Header/Header'
-import Header from  'C:/Web_Development/tomato/frontend/src/components/Header/Header.js'
+import Header from  '../../components/Header/Header.js'
 // import ExploreMenu from 'C:/Web_Development/tomato/frontend/src/components/ExploreMenu/ExploreMenu.js'
-import FoodDisplay from 'C:/Web_Development/tomato/frontend/src/components/FoodDisplay/FoodDisplay.js'
-import AppDownload from 'C:/Web_Development/tomato/frontend/src/components/AppDownload/AppDownload.js'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay.js'
+import AppDownload from '../../components/AppDownload/AppDownload.js'
 
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu.js'
 
