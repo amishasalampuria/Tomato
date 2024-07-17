b4129939144aeb72694a88c9f84ac33dda1d0a9d
amishasalampuria <91007698+amishasalampuria@users.noreply.github.com>
2024-07-17 18:00:09 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import {Routes, Route} from 'react-router-dom'
-import Home from 'C:/Web_Development/tomato/frontend/src/pages/Home/Home.js';
-import Cart from 'C:/Web_Development/tomato/frontend/src/pages/Cart/Cart.js';
+import Home from ''./pages/Home/Home.js'';
+import Cart from './pages/Cart/Cart.js';
 import PlaceOrder  from './pages/PlaceOrder/PlaceOrder.js';
 import NotFound from './pages/NotFound.js';
-import Footer from 'C:/Web_Development/tomato/frontend/src/components/Footer/Footer.js'
+import Footer from './components/Footer/Footer.js'
 import LoginPopUp from './components/LoginPopUp/LoginPopUp.js';
 import MyOrders from './pages/MyOrders/MyOrders.js';
 import { ToastContainer } from 'react-toastify';
